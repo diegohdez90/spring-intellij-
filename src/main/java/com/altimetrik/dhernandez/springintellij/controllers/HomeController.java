4558603ccb01572402e85cac03a24e01f8bd7b99
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-02-14 10:58:19 -0600

    refactor: update package name

--- src/main/java/com/altimetrik/dhernandez/springintellij/controllers/HomeController.java
+++ src/main/java/com/altimetrik/dhernandez/springintellij/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.altimetrik.dhernandez.springintellij.web;
+package com.altimetrik.dhernandez.springintellij.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
