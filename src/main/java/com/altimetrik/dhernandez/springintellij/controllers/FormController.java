4d7b4856d69b5881ac95aa38e34b672fa655cd95
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-02-14 12:39:30 -0600

    refactor: update parameter to servlet request class

--- src/main/java/com/altimetrik/dhernandez/springintellij/controllers/FormController.java
+++ src/main/java/com/altimetrik/dhernandez/springintellij/controllers/FormController.java
@@ -1,8 +1,8 @@
 package com.altimetrik.dhernandez.springintellij.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 @Controller
@@ -14,8 +14,10 @@ public class FormController {
     }
 
     @RequestMapping("/submit")
-    public String getSurveyData(@RequestParam(defaultValue="John Doe") String fullName, Model model) {
-        model.addAttribute("fullName", fullName);
+    public String getSurveyData(HttpServletRequest request, Model model) {
+        String fullName = request.getParameter("fullName");
+        String welcomeMessage = "Welcome to your dashboard " + fullName;
+        model.addAttribute("welcomeMessage", welcomeMessage);
         return "welcome";
     }
 }
