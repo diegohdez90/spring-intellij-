c5f600607a3bada674f62aa0cf0bb5a010dac472
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-02-21 16:39:02 -0600

    refactor: add countries options as hash list

--- src/main/java/com/altimetrik/dhernandez/springintellij/models/Student.java
+++ src/main/java/com/altimetrik/dhernandez/springintellij/models/Student.java
@@ -1,11 +1,23 @@
 package com.altimetrik.dhernandez.springintellij.models;
 
+import java.util.LinkedHashMap;
+
 public class Student {
     private String firstName;
     private String lastName;
     private String country;
+    private LinkedHashMap<String, String> countries;
 
-    public Student() {}
+    public Student() {
+        this.countries = new LinkedHashMap<>();
+        countries.put("United States", "US");
+        countries.put("Canada", "CA");
+        countries.put("Mexico", "MX");
+        countries.put("France", "FR");
+        countries.put("Spain", "SP");
+        countries.put("Brazil", "BR");
+        countries.put("Argentina", "AR");
+    }
 
     public Student(String firstName, String lastName) {
         this.firstName = firstName;
@@ -34,5 +46,9 @@ public class Student {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public LinkedHashMap<String, String> getCountries() {
+        return countries;
     }
 }
