891aac7cff143b8418017a8a28ca928cc3b9ff11
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-02-23 11:34:38 -0600

    feat: add binder to trim string values

--- src/main/java/com/altimetrik/dhernandez/springintellij/controllers/CustomerController.java
+++ src/main/java/com/altimetrik/dhernandez/springintellij/controllers/CustomerController.java
@@ -2,9 +2,12 @@ package com.altimetrik.dhernandez.springintellij.controllers;
 
 import com.altimetrik.dhernandez.springintellij.models.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,5 +30,11 @@ public class CustomerController {
         System.out.println(customer.getFirstName());
         System.out.println(customer.getLastName());
         return "customer-confirmation";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 }
