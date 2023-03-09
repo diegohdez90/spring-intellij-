eeedfdae49e449755903fd7505ee1b7d49c630ab
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-03-09 14:50:10 -0600

    feat: add students formatted template

--- src/main/java/com/altimetrik/dhernandez/springintellij/controllers/PupilController.java
+++ src/main/java/com/altimetrik/dhernandez/springintellij/controllers/PupilController.java
@@ -7,7 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/v1")
@@ -36,5 +39,14 @@ public class PupilController {
     @GetMapping(path = "/list")
     public @ResponseBody Iterable<Pupil> getAll() {
         return service.getAll();
+    }
+
+    @GetMapping("/list-formatted")
+    public String format(Model model) {
+        Iterable<Pupil> students = service.getAll();
+        log.info("You have retrieved");
+        model.addAttribute("students", students);
+        return "student-list";
+
     }
 }
